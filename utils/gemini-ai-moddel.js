354a760623a8f35b2a3948a93aab981cac26f4d4
Pratik <93414233+pratik-git10@users.noreply.github.com>
2026-02-22 17:16:47 +0530

    updated Gemini model

--- utils/gemini-ai-moddel.js
+++ utils/gemini-ai-moddel.js
@@ -1,14 +1,10 @@
-const {
-  GoogleGenerativeAI,
-  HarmCategory,
-  HarmBlockThreshold,
-} = require("@google/generative-ai");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash",
+  model: "gemini-3-flash-preview",
 });
 
 const generationConfig = {
